a62d85c9992292bbc8d13fa921472a74cb4fe607
jeramiah1017 <55998280+jeramiah1017@users.noreply.github.com>
2019-10-31 21:47:23 -0600

    cant

--- src/components/WizardFour/WizardFour.js
+++ src/components/WizardFour/WizardFour.js
@@ -1,5 +1,12 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {updateFound} from '../../ducks/reducer';
+
+class WizardFour extends Component {
+    render() {
+        const { updateFound } = this.props
+
 
 class WizardFour extends Component {
     render(){
@@ -9,13 +16,20 @@ class WizardFour extends Component {
                     <p>Have you already found your new home?</p> <br />
                     
                     <div className="row">
-                        <Link to="/wFive"><button onClick={this.props.foundTrue}>Yes</button></Link>
-                        <Link to="/wFive"><button onClick={this.props.foundFalse}>No </button></Link>  
+                        <Link to="/wFive"><button onClick={( e ) => updateFound( true )}>Yes</button></Link>
+                        <Link to="/wFive"><button onClick={( e ) => updateFound( true )}>No </button></Link>  
                     </div>         
                 </div>
             </div>
         )
     }
-}
 
-export default WizardFour;
+}
+function mapStateToProps( state ) {
+    const { found } = state;
+  
+    return {
+      found
+    };
+  }
+export default connect( mapStateToProps, { updateFound })(WizardFour);
--- src/components/WizardThree/WizardThree.js
+++ src/components/WizardThree/WizardThree.js
@@ -8,14 +8,21 @@ class WizardThree extends Component {
                 <div className="vert-align">
                    <p> What property are you looking to use the loan on? </p><br />
                     <div className="row">
-                        <Link to="/wFour"><button value="primaryHome" onClick={this.props.propToBeUsedOn}>Primary Home</button></Link>
-                        <Link to="/wFour"><button value="rentalProperty" onClick={this.props.propToBeUsedOn}>Rental Property</button></Link>
-                        <Link to="/wFour"><button value="secondaryHome" onClick={this.props.propToBeUsedOn}>Secondary Home</button></Link>
+                        <Link to="/wFour"><button value="primaryHome" onClick={ ( e ) => updateProp( e.target.value )>Primary Home</button></Link>
+                        <Link to="/wFour"><button value="rentalProperty"onClick={ ( e ) => updateProp( e.target.value )>Rental Property</button></Link>
+                        <Link to="/wFour"><button value="secondaryHome" onClick={ ( e ) => updateProp( e.target.value )>Secondary Home</button></Link>
                     </div>
                 </div>           
             </div>
         )
     }
 }
+function mapStateToProps( state ) {
+    const {propToBeUsedOn} = state;
+    return {
+        propToBeUsedOn
+    }
+}
 
-export default WizardThree; 
+export default connect( mapStateToProps , {updateProp})(WizardThree); 
+ 
